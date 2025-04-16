3e7ac3c8831b6fe76d9ecba7198f96555f50c303
Khalid El-MariAa <khalidmaria13@gmail.com>
2025-04-16 17:58:35 +0200

    app first step

---
+++ lib/features/registrartion/presentation/view/registration_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RegistrartionScreen extends StatelessWidget {
+  const RegistrartionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      
+    ));
+  }
+}
